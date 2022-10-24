3f61c051eaf3bcadbbf2896c67a391427862afbb
Suporte <Suporte@ESN602N1243068.educ602.local>
2022-10-24 17:23:47 -0300

    first

---
+++ Data/Contexto.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace pdf_final.Data
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options): base(options)
+        {
+
+        }
+    }
+}
---
+++ Models/TipoCurso.cs
@@ -0,0 +1,11 @@
+﻿namespace pdf_final.Models
+{
+    public class TipoCurso
+    {
+        public int TipoCursoId { get; set; }
+
+        public string Tipo { get; set; }
+
+        public ICollection<FormacaoAcademica> FormacoesAcademicas { get; set; }
+    }
+}
